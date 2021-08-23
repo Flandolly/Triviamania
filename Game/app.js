bdb85c9ac6f55c84c4700df4120b49fe5f15773e
christina-whethers <flandresamarawr@gmail.com>
2021-08-23 11:12:59 -0400

    Clustered backend for modal together for better code organization

--- Game/app.js
+++ Game/app.js
@@ -1,74 +1,78 @@
-const openModalButton = document.getElementById("modalBtn")
-const closeModalButton = document.getElementById("close")
-const nextButton = document.getElementById("next")
-const prevButton = document.getElementById("prev")
-const modal = document.getElementById("modal")
-
-let pageIndex = 0
 /*
 Functionality for Tutorial modal
  */
-function openModal() {
-    // changePage("def")
-    return modal.style.display = "block"
-}
 
-function closeModal() {
-    changePage("def")
-    return modal.style.display = "none"
-}
+{
+    const openModalButton = document.getElementById("modalBtn")
+    const closeModalButton = document.getElementById("close")
+    const nextButton = document.getElementById("next")
+    const prevButton = document.getElementById("prev")
+    const modal = document.getElementById("modal")
 
-function changePage(buttonId) {
-    const modalP1 = document.querySelector(".p1")
-    const modalP2 = document.querySelector(".p2")
-    const modalP3 = document.querySelector(".p3")
+    let pageIndex = 0
 
-    switch (buttonId) {
-        case "next":
-            pageIndex++
-            break
-        case "prev":
-            pageIndex--
-            break
-        default:
-            pageIndex = 0
-            break
+    function openModal() {
+        // changePage("def")
+        return modal.style.display = "block"
     }
 
-    switch (pageIndex) {
-        case 0:
-            nextButton.style.display = "inline-block"
-            prevButton.style.display = "none"
-            modalP1.style.display = "block"
-            modalP2.style.display = "none"
-            modalP3.style.display = "none"
-            break
-        case 1:
-            prevButton.style.display = "inline-block"
-            nextButton.style.display = "inline-block"
-            modalP1.style.display = "none"
-            modalP2.style.display = "inline-block"
-            modalP3.style.display = "none"
-            break
-        case 2:
-            prevButton.style.display = "inline-block"
-            nextButton.style.display = "none"
-            modalP1.style.display = "none"
-            modalP2.style.display = "none"
-            modalP3.style.display = "block"
-            break
+    function closeModal() {
+        changePage("def")
+        return modal.style.display = "none"
     }
-}
 
-openModalButton.addEventListener("click", function() {
-    openModal()
-})
-closeModalButton.addEventListener("click", function() {
-    closeModal()
-})
-nextButton.addEventListener("click", function() {
-    changePage(nextButton.id)
-})
-prevButton.addEventListener("click", function() {
-    changePage(prevButton.id)
-})
+    function changePage(buttonId) {
+        const modalP1 = document.querySelector(".p1")
+        const modalP2 = document.querySelector(".p2")
+        const modalP3 = document.querySelector(".p3")
+
+        switch (buttonId) {
+            case "next":
+                pageIndex++
+                break
+            case "prev":
+                pageIndex--
+                break
+            default:
+                pageIndex = 0
+                break
+        }
+
+        switch (pageIndex) {
+            case 0:
+                nextButton.style.display = "inline-block"
+                prevButton.style.display = "none"
+                modalP1.style.display = "block"
+                modalP2.style.display = "none"
+                modalP3.style.display = "none"
+                break
+            case 1:
+                prevButton.style.display = "inline-block"
+                nextButton.style.display = "inline-block"
+                modalP1.style.display = "none"
+                modalP2.style.display = "inline-block"
+                modalP3.style.display = "none"
+                break
+            case 2:
+                prevButton.style.display = "inline-block"
+                nextButton.style.display = "none"
+                modalP1.style.display = "none"
+                modalP2.style.display = "none"
+                modalP3.style.display = "block"
+                break
+        }
+    }
+
+    openModalButton.addEventListener("click", function() {
+        openModal()
+    })
+    closeModalButton.addEventListener("click", function() {
+        closeModal()
+    })
+    nextButton.addEventListener("click", function() {
+        changePage(nextButton.id)
+    })
+    prevButton.addEventListener("click", function() {
+        changePage(prevButton.id)
+    })
+}
